31537cec634e4643c9d932006000ef29542b3114
sharatham1993 <sharatham1993@gmail.com>
2023-01-05 20:18:47 +0530

    first commit

---
+++ src/test/java/com/newProject1/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newProject1.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
